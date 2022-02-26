f9bb246faf134b9d804ea44f360a528b8c4b5527
Niniatura <neli.marcos@gmail.com>
2022-02-26 07:40:39 -0300

    Tabla con datos

--- components/componente1.js
+++ components/componente1.js
@@ -1,12 +1,20 @@
 Vue.component('componente1', {
     template: //html
     `
-    <table>
-        <tr v-for="(item, i) in array">
-            <td>{{item}}</td>
-            <td>{{boolean}}</td>
-        </tr>
+    <table class="table">
+        <thead>
+            <tr>
+            <th scope="col">Array</th>
+            <th scope="col">Boolean</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="(item, i) in array">
+                <td>{{item}}</td>
+                <td>{{boolean}}</td>
+            </tr>
+        </tbody>
     </table>
     `,
-    props:['array','style','boolean']
+    props:['array','MyStyle','boolean']
 })
